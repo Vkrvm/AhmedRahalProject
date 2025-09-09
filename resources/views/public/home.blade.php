9abc3dc1620ec8fe8303278781cd2083177ba803
Vkrvm <mahakram35@gmail.com>
2025-09-09 17:29:18 +0300

    feat(home): add Swiper slider markup to homepage

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -9,7 +9,19 @@
 			<div class="hero-overlay">
 				<h1>Designing Your Space, Defining Your Style</h1>
 				<p>Elegant. Timeless. Functional.</p>
-                <a href="{{ route('projects') }}" class="btn btn-expolre">Explore Our Projects</a>
+				<a href="{{ route('projects') }}" class="btn btn-expolre">Explore Our Projects</a>
+			</div>
+		</div>
+
+		<div class="imgs-slider">
+			<div class="mySwiper swiper">
+				<div class="swiper-wrapper">
+					@foreach ($sliderImages as $img)
+						<div class="swiper-slide">
+							<img src="{{ asset($img->image_path) }}" alt="{{ $img->title }}" class="img-fluid w-100" />
+						</div>
+					@endforeach
+				</div>
 			</div>
 		</div>
 	</section>
