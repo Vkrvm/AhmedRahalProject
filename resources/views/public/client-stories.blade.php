4d07b3f9b0fecdad69c780993d099b103541ddea
Vkrvm <mahakram35@gmail.com>
2025-09-15 00:49:21 +0300

    feat(views): add testimonial cards to clinet-stories view with responsive grid layouts

--- resources/views/public/client-stories.blade.php
+++ resources/views/public/client-stories.blade.php
@@ -2,7 +2,123 @@
 
 @section('content')
 	<section class="page page-client-stories">
-		<h1>Client Stories</h1>
-		<p>Client Stories page content.</p>
+        <div class="container">
+            <h1>Client Stories</h1>
+            <p class="text-center">Client Stories page content.</p>
+            <div class="row">
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 mb-3">
+                    <div class="card shadow p-3">
+                        <div class="personal-data">
+                            <img src="{{ Vite::asset('resources/images/1.jpg') }}" alt="">
+                            <div class="name"> <h4>Ahmed Rahal</h4> </div>
+                        </div>
+                        <div class="quote mt-3">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, omnis delectus soluta voluptas incidunt expedita accusamus ab minima consectetur quam quae esse fugit hic tempora ratione vel at quaerat quas.</p>
+                        </div>
+                        <div class="project-btn">
+                            <a class="btn" href="">See Project</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</section>
 @endsection
